f3edeb8db878517bcf2bcdf54d020a5193af465e
XavierBradley <xavbrdl@gmail.com>
2026-02-18 14:42:25 -0500

    Making the git

---
+++ src/test/java/com/champsoft/vrms1730298/Vrms1730298ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.champsoft.vrms1730298;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vrms1730298ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
