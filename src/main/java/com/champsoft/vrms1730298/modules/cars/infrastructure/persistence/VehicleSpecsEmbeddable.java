63c2414e5548d0aaa109acfb472f95592f3fa1e0
XavierBradley <xavbrdl@gmail.com>
2026-03-12 12:17:12 -0400

    Added VehicleSpecsEmbeddable

---
+++ src/main/java/com/champsoft/vrms1730298/modules/cars/infrastructure/persistence/VehicleSpecsEmbeddable.java
@@ -0,0 +1,26 @@
+package com.champsoft.vrms1730298.modules.cars.infrastructure.persistence;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class VehicleSpecsEmbeddable {
+
+    @Column(name = "make", nullable = false)
+    public String make;
+
+    @Column(name = "model", nullable = false)
+    public String model;
+
+    @Column(name = "vehicle_year", nullable = false)
+    public int year;
+
+    protected VehicleSpecsEmbeddable() {
+    }
+
+    public VehicleSpecsEmbeddable(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+}
